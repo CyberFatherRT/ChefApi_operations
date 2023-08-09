bcff12d96fbb7adae7c8a14f4d75288d184a6ec4
GodfatherRT <maks12ate@gmail.com>
2023-08-09 21:07:38 +0300

    add AnalyseHash operation

--- ciphers/src/lib.rs
+++ ciphers/src/lib.rs
@@ -5,6 +5,7 @@ pub use operations::a1z26cipher_decode_mod::A1Z26CipherDecode;
 pub use operations::a1z26cipher_encode_mod::A1Z26CipherEncode;
 pub use operations::affine_cipher_decode_mod::AffineCipherDecode;
 pub use operations::affine_cipher_encode_mod::AffineCipherEncode;
+pub use operations::analyse_hash_mod::AnalyseHash;
 pub use operations::from_base64_mod::FromBase64;
 pub use operations::to_base64_mod::ToBase64;
 pub use operations::vigenere_cipher_decode_mod::VigenereCipherDecode;
