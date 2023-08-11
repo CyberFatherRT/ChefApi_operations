4b133ad9e050ca5ef3b7169d4069b46eb80ffd60
GodfatherRT <maks12ate@gmail.com>
2023-08-11 12:31:56 +0300

    correct info struct

--- ciphers/src/operations/argon2_mod.rs
+++ ciphers/src/operations/argon2_mod.rs
@@ -212,20 +212,20 @@ enum OutputFormat {
 
 #[derive(Serialize)]
 pub struct Argon2Info {
-    pub name: &'static str,
-    pub module: &'static str,
-    pub description_en: Option<&'static str>,
-    pub description_ru: Option<&'static str>,
-    pub info_url: Option<&'static str>,
+    name: &'static str,
+    documentation: &'static str,
+    description_en: &'static str,
+    description_ru: &'static str,
+    info_url: Option<&'static str>,
 }
 
 impl Default for Argon2Info {
     fn default() -> Self {
         Self {
             name: "Argon2",
-            module: "Crypto",
-            description_en: Some("Argon2 is a key derivation function that was selected as the winner of the Password Hashing Competition in July 2015. It was designed by Alex Biryukov, Daniel Dinu, and Dmitry Khovratovich from the University of Luxembourg.<br><br>Enter the password in the input to generate its hash."),
-            description_ru: Some("Argon2 – это функция получения ключа, которая была выбрана победителем конкурса хеширования паролей в июле 2015 года. Она была разработана Алексом Бирюковым, Даниэлем Дину и Дмитрием Ховратовичем из Люксембургского университета.<br><br>Введите пароль в ввод для генерации его хэша."),
+            documentation: "soon I transfer all documentation to somewhere :/",
+            description_en: "Argon2 is a key derivation function that was selected as the winner of the Password Hashing Competition in July 2015. It was designed by Alex Biryukov, Daniel Dinu, and Dmitry Khovratovich from the University of Luxembourg.<br><br>Enter the password in the input to generate its hash.",
+            description_ru: "Argon2 – это функция получения ключа, которая была выбрана победителем конкурса хеширования паролей в июле 2015 года. Она была разработана Алексом Бирюковым, Даниэлем Дину и Дмитрием Ховратовичем из Люксембургского университета.<br><br>Введите пароль в ввод для генерации его хэша.",
             info_url: Some("https://wikipedia.org/wiki/Argon2"),
         }
     }
