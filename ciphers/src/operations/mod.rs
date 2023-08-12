1cb2aefe68c7db264a712c155abb544786dbb073
GodfatherRT <maks12ate@gmail.com>
2023-08-12 19:23:53 +0300

    test for GHA

--- ciphers/src/operations/mod.rs
+++ ciphers/src/operations/mod.rs
@@ -1,15 +1,1 @@
-pub mod a1z26_cipher_decode_mod;
-pub mod a1z26_cipher_encode_mod;
-pub mod affine_cipher_decode_mod;
-pub mod affine_cipher_encode_mod;
-pub mod analyse_hash_mod;
-pub mod argon2_compare_mod;
-pub mod argon2_mod;
-pub mod atbash_cipher_mod;
-pub mod bcrypt_compare_mod;
-pub mod bcrypt_mod;
-pub mod bcrypt_parse_mod;
-pub mod from_base64_mod;
-pub mod to_base64_mod;
-pub mod vigenere_cipher_decode_mod;
-pub mod vigenere_cipher_encode_mod;
+pub mod a1z26_cipher_decode_mod;pub mod a1z26_cipher_encode_mod;pub mod affine_cipher_decode_mod;pub mod affine_cipher_encode_mod;pub mod analyse_hash_mod;pub mod argon2_compare_mod;pub mod argon2_mod;pub mod atbash_cipher_mod;pub mod bcrypt_compare_mod;pub mod bcrypt_mod;pub mod bcrypt_parse_mod;pub mod from_base64_mod;pub mod to_base64_mod;pub mod vigenere_cipher_decode_mod;pub mod vigenere_cipher_encode_mod;
