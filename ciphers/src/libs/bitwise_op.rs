befdc7962361d8d176adab097ddf11c37512dee2
GodfatherRT <maks12ate@gmail.com>
2023-08-13 12:25:09 +0300

    create bit_op funtion that run bitwise operations across the input data

--- ciphers/src/libs/bitwise_op.rs
+++ ciphers/src/libs/bitwise_op.rs
@@ -1,51 +1,52 @@
-use num::Integer;
-use std::ops::{Add, BitAnd, BitOr, BitXor, Not, Rem, Sub};
+use serde::{Deserialize, Serialize};
 
-pub fn xor<T, V, R>(operand: T, key: V) -> R
+pub fn bit_op<F>(input: Vec<u8>, key: Vec<u8>, func: F) -> Vec<u8>
 where
-    T: BitXor<V, Output = R>,
+    F: Fn(u8, u8) -> u8,
 {
+    input
+        .iter()
+        .enumerate()
+        .map(|(i, &e)| func(e, key[i % key.len()]))
+        .collect()
+}
+
+pub fn xor(operand: u8, key: u8) -> u8 {
     operand ^ key
 }
 
-pub fn not<T, V, R>(operand: T, _: V) -> R
-where
-    T: Not<Output = T> + BitAnd<isize, Output = R>,
-{
-    !operand & 0xff
+pub fn not(operand: u8, _: u8) -> u8 {
+    (!operand as i16 & 0xffi16) as u8
 }
 
-pub fn and<T, V, R>(operand: T, key: V) -> R
-where
-    T: BitAnd<V, Output = R>,
-{
+pub fn and(operand: u8, key: u8) -> u8 {
     operand & key
 }
 
-pub fn or<T>(operand: T, key: T) -> T
-where
-    T: BitOr<Output = T>,
-{
+pub fn or(operand: u8, key: u8) -> u8 {
     operand | key
 }
 
-pub fn add<T, V, R>(operand: T, key: V) -> R
-where
-    T: Add<V, Output = R>,
-    R: Rem<isize, Output = R>,
-{
-    (operand + key) % 256
+pub fn add(operand: u8, key: u8) -> u8 {
+    ((operand as i16 + key as i16) % 256) as u8
 }
 
-pub fn sub<T, V, R>(operand: T, key: V) -> R
-where
-    T: Sub<V, Output = R>,
-    R: Integer + Add<u16, Output = R>,
-{
-    let result = operand - key;
-    if result < R::zero() {
-        result + 256
+pub fn sub(operand: u8, key: u8) -> u8 {
+    let result = operand as i16 - key as i16;
+    if result < 0 {
+        (result + 256) as u8
     } else {
-        result
+        result as u8
     }
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum BitwiseOpDelimiters {
+    Hex,
+    Decimal,
+    Binary,
+    Base64,
+    Utf8,
+    Latin1,
 }
