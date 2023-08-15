64945fb2c85ae89fd022e0118a4fb05d0e38df4b
GodfatherRT <maks12ate@gmail.com>
2023-08-15 15:18:13 +0300

    Create MD4 function

--- ciphers/src/lib.rs
+++ ciphers/src/lib.rs
@@ -19,6 +19,7 @@ pub use operations::bcrypt_parse_mod::{BcryptParse, BcryptParseInfo};
 pub use operations::bifid_cipher_encode_mod::{BifidCipherEncode, BifidCipherEncodeInfo};
 pub use operations::from_base64_mod::{FromBase64, FromBase64Info};
 pub use operations::md2_mod::{MD2Info, MD2};
+pub use operations::md4_mod::{MD4Info, MD4};
 pub use operations::rsa_decode_mod::{RSADecrypt, RSADecryptInfo};
 pub use operations::rsa_encode_mod::{RSAEncrypt, RSAEncryptInfo};
 pub use operations::to_base64_mod::{ToBase64, ToBase64Info};
@@ -75,6 +76,7 @@ pub enum Operations {
     BifidCipherEncode,
     FromBase64,
     MD2,
+    MD4,
     RSADecrypt,
     RSAEncrypt,
     ToBase64,
