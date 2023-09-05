3e1f2156a9a20604664f37c88ff20c462bf7504c
cyberfather <maks12ate@gmail.com>
2023-09-05 19:35:16 +0000

    delete `todo!()` that cause error in runtime

--- src/main.rs
+++ src/main.rs
@@ -108,6 +108,5 @@ async fn cipher_help_handler(name: Path<Operations>) -> HttpResponse {
     };
     HttpResponse::build(StatusCode::OK)
         .content_type("application/json; charset=utf-8")
-        .body(res);
-    todo!()
+        .body(res)
 }
