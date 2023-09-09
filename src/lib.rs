dda1d7995f6cfbb292cf242fc77db4b41c8c46d0
cyberfather <maks12ate@gmail.com>
2023-09-09 16:01:05 +0000

    some change for library

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,13 @@
 mod operations;
+
+use serde::{Deserialize, Serialize};
 pub use operations::*;
+use utils::Operation;
+
+pub fn run_operations<'a, I, O>(operations: impl Operation<'a, I, O>, request: &str) -> Result<O, String>
+    where
+        I: Deserialize<'a>,
+        O: Serialize,
+{
+    operations.do_black_magic(request)
+}
